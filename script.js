c36f98d8b78d1fcd0e7743d6ae450d15910a49ff
alexandreratuchneribas-oss <alexandre.ratuchne.ribas@escola.pr.gov.br>
2026-05-12 08:06:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const areas = [
+    {nome:"Área 1", umidade:22, nitrogenio:15, fosfato:8},
+    {nome:"Área 2", umidade:18, nitrogenio:10, fosfato:5},
+    {nome:"Área 3", umidade:25, nitrogenio:20, fosfato:10},
+    {nome:"Área 4", umidade:20, nitrogenio:12, fosfato:7},
+    {nome:"Área 5", umidade:23, nitrogenio:18, fosfato:9},
+];
 
+const selectArea = document.getElementById('selectArea');
+const umidadeEl = document.getElementById('umidade');
+const
